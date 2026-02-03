16518032109674135ab80c03ffecb4c95ee9760d
Sergio Xalambrí <hello@sergiodxa.com>
2026-02-03 18:43:47 -0500

    style(blog): reorder react-router.config.ts future flags

--- apps/blog/react-router.config.ts
+++ apps/blog/react-router.config.ts
@@ -2,10 +2,10 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
 	future: {
-		v8_middleware: true,
 		unstable_optimizeDeps: true,
-		v8_splitRouteModules: true,
 		unstable_subResourceIntegrity: true,
+		v8_middleware: true,
+		v8_splitRouteModules: true,
 		v8_viteEnvironmentApi: true,
 	},
 } satisfies Config;
