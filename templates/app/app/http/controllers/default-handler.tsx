cd183b6f9fc18df50e114735fc96226ff17e1773
Sergio Xalambrí <hello@sergiodxa.com>
2026-05-06 22:53:17 -0500

    feat: update app template for Remix client entry hydration

--- templates/app/app/http/controllers/default-handler.tsx
+++ templates/app/app/http/controllers/default-handler.tsx
@@ -2,9 +2,18 @@ import action from "@pkg/remix-helpers/action";
 import view from "@pkg/remix-helpers/view";
 
 import NotFoundViewModel from "~/app/http/view-models/not-found";
+import { Counter } from "~/resources/components/timer";
+import DocumentLayout from "~/resources/layouts/document";
 import NotFoundView from "~/resources/views/not-found";
 
 export default action(() => {
-	let setup = NotFoundViewModel.default({ title: "Page Not Found" });
-	return view(<NotFoundView setup={setup} />, { status: 404 });
+	let props = NotFoundViewModel.default({ title: "Page Not Found" });
+
+	return view(
+		<DocumentLayout title={props.title}>
+			<NotFoundView {...props} />
+			<Counter />
+		</DocumentLayout>,
+		{ status: 404 },
+	);
 });
--- templates/app/resources/views/not-found.tsx
+++ templates/app/resources/views/not-found.tsx
@@ -3,10 +3,10 @@ import type { Handle } from "remix/ui";
 import type { NotFoundViewModel } from "~/app/http/view-models/not-found";
 
 namespace NotFoundView {
-	export interface Props extends NotFoundViewModel.DefaultOutput {}
+	export interface Setup extends NotFoundViewModel.DefaultOutput {}
 }
 
-export default function NotFoundView(handle: Handle<NotFoundView.Props>) {
+export default function NotFoundView(handle: Handle<NotFoundView.Setup>) {
 	return () => {
 		return <h1>{handle.props.title}</h1>;
 	};
