23f8ac3c824ce5ddcd4f1d1c509f4468c0019795
Sergio Xalambrí <hello@sergiodxa.com>
2026-06-11 17:20:36 -0500

    chore(blog): enable all React Router future flags

--- apps/blog/react-router.config.ts
+++ apps/blog/react-router.config.ts
@@ -3,9 +3,11 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	future: {
 		unstable_optimizeDeps: true,
-		unstable_subResourceIntegrity: true,
+		unstable_previewServerPrerendering: true,
 		v8_middleware: true,
+		v8_passThroughRequests: true,
 		v8_splitRouteModules: true,
+		v8_trailingSlashAwareDataRequests: true,
 		v8_viteEnvironmentApi: true,
 	},
 } satisfies Config;
