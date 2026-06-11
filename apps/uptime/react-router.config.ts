fc8e7d1975775eeee77891b0281b320de75d5f89
Sergio Xalambrí <hello@sergiodxa.com>
2026-06-11 18:20:22 -0500

    chore(uptime): enable all feature flags

--- apps/uptime/react-router.config.ts
+++ apps/uptime/react-router.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+	subResourceIntegrity: true,
 	future: {
 		unstable_optimizeDeps: true,
-		unstable_subResourceIntegrity: true,
+		unstable_previewServerPrerendering: true,
 		v8_middleware: true,
+		v8_passThroughRequests: true,
 		v8_splitRouteModules: true,
+		v8_trailingSlashAwareDataRequests: true,
 		v8_viteEnvironmentApi: true,
 	},
 } satisfies Config;
