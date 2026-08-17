01f2114cb0045c718e7e596a596e9716f91114a0
Sergio Xalambrí <hello@sergiodxa.com>
2026-08-17 01:08:07 -0500

    test(uptime): run the transactional email tests under Vitest

--- apps/uptime/app/emails/trial-repeat-report.test.tsx
+++ apps/uptime/app/emails/trial-repeat-report.test.tsx
@@ -11,7 +11,7 @@
  * @copyright Sergio Xalambrí 2026
  */
 
-import { describe, expect, test } from "bun:test";
+import { describe, expect, test } from "vitest";
 
 import { render } from "@pkg/mail";
 
